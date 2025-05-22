9d3135c8e2db1c4c9f74c0207fc692bb1ac44f77
Bryanbatista31 <elln3117@gmail.com>
2025-05-22 09:02:31 -0400

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -1,8 +1,15 @@
 // menu-data.js
 const menuData = {
   "menu": [
-    { "id": 1, "nombre": "Programación 3", "enlace": "https://github.com/Bryanbatista31" },
-    { "id": 2, "nombre": "Sobre Nosotros", "enlace": "/sobre-nosotros" },
+    { "id": 1, "nombre": "Repositorio GitHub", "enlace": "https://github.com/Bryanbatista31" },
+    { "id": 2,
+      "nombre": "Sobre Mí",
+      "enlace": "/sobre-nosotros",
+      "submenus": [
+        { "nombre": "Página web personal en HTML", "enlace": "https://bryanbatista31.github.io/bryanbatista.github.io/"},
+        { "nombre": "Página HTML plain, con su Biografía (Incluir foto)", "enlace": "https://bryanbatista31.github.io/biografiaconfoto.github.io/"}
+      ]
+    },
     {
       "id": 3,
       "nombre": "Servicios",
@@ -15,3 +22,4 @@ const menuData = {
     { "id": 4, "nombre": "Contacto", "enlace": "/contacto" }
   ]
 };
+
