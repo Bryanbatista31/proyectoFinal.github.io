82cf5765aa54e41cb1a0d185ea20634bf79f422e
Bryanbatista31 <elln3117@gmail.com>
2025-05-22 09:38:34 -0400

    Agregar PDFs

--- menu-data.js
+++ menu-data.js
@@ -4,7 +4,7 @@ const menuData = {
     { "id": 1, "nombre": "Repositorio GitHub", "enlace": "https://github.com/Bryanbatista31" },
     { "id": 2,
       "nombre": "Sobre Mí",
-      "enlace": "/sobre-nosotros",
+      "enlace": "https://github.com/Bryanbatista31",
       "submenus": [
         { "nombre": "Página web personal en HTML", "enlace": "https://bryanbatista31.github.io/bryanbatista.github.io/"},
         { "nombre": "Página HTML plain, con su Biografía (Incluir foto)", "enlace": "https://bryanbatista31.github.io/biografiaconfoto.github.io/"}
@@ -12,14 +12,37 @@ const menuData = {
     },
     {
       "id": 3,
-      "nombre": "Servicios",
+      "nombre": "HTML y CSS",
       "enlace": "https://github.com/Bryanbatista31",
       "submenus": [
-        { "nombre": "Consultoría", "enlace": "/servicios/consultoria" },
-        { "nombre": "Desarrollo", "enlace": "/servicios/desarrollo" }
+        { "nombre": "102 Etiquetas HTML", "enlace": "https://bryanbatista31.github.io/etiquetashtml.github.io/" },
+        { "nombre": "Estructura de menús con CSS", "enlace": "https://bryanbatista31.github.io/estructurademenu.github.io/" }
       ]
     },
-    { "id": 4, "nombre": "Contacto", "enlace": "/contacto" }
+    {
+      "id": 3,
+      "nombre": "Javascript",
+      "enlace": "https://github.com/Bryanbatista31",
+      "submenus": [
+        { "nombre": "Algoritmo de módulo 10", "enlace": "https://bryanbatista31.github.io/algoritmoLuhn.github.io/" },
+        { "nombre": "Menú dinámico", "enlace": "https://bryanbatista31.github.io/menuDinamico.github.io/" },
+        { "nombre": "Formulario múltiples páginas", "enlace": "https://bryanbatista31.github.io/FormularioMultiplesPaginas.github.io/" },
+        { "nombre": "Formulario de contacto QR (vcard)", "enlace": "https://bryanbatista31.github.io/codigoqr.github.io/" },
+        { "nombre": "Librería de gráficos", "enlace": "https://bryanbatista31.github.io/formularioLibreriaGraficos.github.io/" }
+      ]
+    },
+    {
+      "id": 3,
+      "nombre": "Resúmenes",
+      "enlace": "https://github.com/Bryanbatista31",
+      "submenus": [
+        { "nombre": "XML", "enlace": "" },
+        { "nombre": "JSON", "enlace": "" },
+        { "nombre": "AJAX", "enlace": "" },
+        { "nombre": "MVC", "enlace": "" }
+      ]
+    },
+    { "id": 4, "nombre": "Servicio Web y Database", "enlace": "/contacto" }
   ]
 };
 
